549dc37648beda6aab187031e617e0ee32b3813c
blimika <iputudyatmika@gmail.com>
2022-02-15 14:11:14 +0800

    v1.2.5
    perbaikan verifycsrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,6 +3,8 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+use Illuminate\Foundation\Application;
+use Illuminate\Contracts\Encryption\Encrypter;
 
 class VerifyCsrfToken extends Middleware
 {
@@ -11,9 +13,16 @@ class VerifyCsrfToken extends Middleware
      *
      * @var array
      */
-
+    /*
     protected $except = [
         //
         '/AAGXPC-OdmO689yu5-L0iN4mhQ-icF0xDbo/webhook',
     ];
+    */
+    public function __construct(Application $app, Encrypter $encrypter) {
+        parent::__construct($app, $encrypter);
+        $this->except = [
+           '/'.env("TELEGRAM_HASH_URL").'/webhook'
+        ];
+    }
 }
